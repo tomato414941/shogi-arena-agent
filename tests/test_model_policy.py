0fe679d13c58f73d1907bc2848a202bb3d113d61
tomato414941 <tomato414941@gmail.com>
2026-05-19 12:05:01 +0000

    refactor: delegate checkpoint inference to intrep

--- tests/test_model_policy.py
+++ tests/test_model_policy.py
@@ -1,12 +1,5 @@
-import tempfile
 import unittest
 from collections.abc import Sequence
-from pathlib import Path
-
-import torch
-
-from shogi_arena_agent.mcts_config import MctsConfig
-from shogi_arena_agent.mcts_move_selector import MctsMoveSelector
 from shogi_arena_agent.shogi_game import play_shogi_game
 from shogi_arena_agent.model_policy import DirectMovePolicy, RankedMovePolicy, ShogiMoveChoiceCheckpointEvaluator
 from shogi_arena_agent.usi import UsiEngine, UsiPosition, board_from_position
@@ -95,43 +88,6 @@ class RankedMovePolicyTest(unittest.TestCase):
         move = DirectMovePolicy(evaluator).select_move(UsiPosition())
 
         self.assertEqual(move, "7g7f")
-
-    def test_policy_plane_checkpoint_evaluator_feeds_mcts_priors(self) -> None:
-        from intrep.problems.shogi_policy_value.checkpoint import save_shogi_policy_value_model_checkpoint
-        from intrep.problems.shogi_policy_value.model import SHOGI_POLICY_VALUE_MODEL_POLICY_PLANE_SHARED_TRANSFORMER
-        from intrep.problems.shogi_policy_value.training import ShogiPolicyValueTrainingConfig, build_shogi_policy_value_model
-        from intrep.worlds.shogi.policy_plane import shogi_policy_plane_action_index
-
-        position = UsiPosition()
-        board = board_from_position(position)
-        legal_moves = tuple(sorted(move.usi() for move in board.legal_moves))
-        preferred_move = "7g7f"
-        action_index = shogi_policy_plane_action_index(preferred_move, turn=board.turn)
-        config = ShogiPolicyValueTrainingConfig(
-            model=SHOGI_POLICY_VALUE_MODEL_POLICY_PLANE_SHARED_TRANSFORMER,
-            embedding_dim=8,
-            hidden_dim=16,
-            num_heads=2,
-            num_layers=1,
-        )
-        model = build_shogi_policy_value_model(config)
-        with torch.no_grad():
-            for parameter in model.parameters():
-                parameter.zero_()
-            model.policy_head.scorer[-1].bias[action_index] = 8.0
-
-        with tempfile.TemporaryDirectory() as directory:
-            checkpoint_path = Path(directory) / "policy-plane.pt"
-            save_shogi_policy_value_model_checkpoint(checkpoint_path, model, config)
-            evaluator = ShogiMoveChoiceCheckpointEvaluator.from_checkpoint(checkpoint_path)
-
-        priors, value = evaluator.evaluate(board, legal_moves)
-        selected = MctsMoveSelector(evaluator, config=MctsConfig(simulation_count=8)).select_move(position)
-
-        self.assertEqual(max(priors, key=priors.get), preferred_move)
-        self.assertAlmostEqual(sum(priors.values()), 1.0, places=6)
-        self.assertEqual(value, 0.0)
-        self.assertEqual(selected, preferred_move)
 
 
 if __name__ == "__main__":
